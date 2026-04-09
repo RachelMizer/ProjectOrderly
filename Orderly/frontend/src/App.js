64f728f50ae068f15d8941fd7648a086d1d61d21
Rachel Mizer <ramizer@my.waketech.edu>
2026-04-09 19:50:17 -0400

    "Checking on branch health and cleaning up some styling issues"

--- Orderly/frontend/src/App.js
+++ Orderly/frontend/src/App.js
@@ -176,8 +176,6 @@ async function fetchCartCount() {
             <Link to="/register">Register</Link>
             {" | "}
             <Link to="/login">Login</Link>
-            {" | "}
-            <Link to="/password-reset">Forgot Password</Link>
           </>
         )}
 
